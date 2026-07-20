65ee0c25d3f4a4e2cad7ea14e21139c10f3aafce
Hinph <hinpher@gmail.com>
2026-07-20 02:27:40 -0700

    Delete src directory

--- src/virtual_mouse.h
+++
@@ -1,7 +0,0 @@
-#ifndef SCWRAPPER_VIRTUAL_MOUSE_H
-#define SCWRAPPER_VIRTUAL_MOUSE_H
-
-int virtual_mouse_setup(int* const uinput_fd);
-int virtual_mouse_destroy(int uinput_fd);
-
-#endif // SCWRAPPER_VIRTUAL_MOUSE_H
